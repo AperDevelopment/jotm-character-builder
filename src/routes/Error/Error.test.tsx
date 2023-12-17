26fae5988cac13eec021987a8416e4f324957f5e
Loïc Dornel <creeperstone72.redstoner@gmail.com>
2023-12-17 15:56:24 +0100

    Removed tests

--- src/routes/Error/Error.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Error from './Error';
-
-test('renders learn react link', () => {
-	render(<Error />);
-	const linkElement = screen.getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
-});
